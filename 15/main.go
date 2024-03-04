ea6234f14e36e759ede9e4e01149e8391196a5cf
ourluv <aorlowde@gmail.com>
2024-03-04 08:23:45 +0300

    finished 15 and 22 tasks

--- 15/main.go
+++ 15/main.go
@@ -9,24 +9,19 @@ import (
 func createHugeString(size int) string {
 	var b strings.Builder
 	for i := 0; i < size; i++ {
-		fmt.Fprint(&b, "🚗")
+		fmt.Fprint(&b, "⛄")
 	}
 	return b.String()
 }
 
-var justString string
-
 func someFunc() {
+	// создаём огромную строку
 	v := createHugeString(1 << 10)
-	// в данном случае мы срезаем по количеству байт, а не по количеству рун
-	justString = v[:100]
 
-	// преобразовываем строку в слайс рун
-	r := []rune(v)
-	// в даннам случае мы срезаем по количеству рун
-	justString = string(r[:100])
+	justString := string(v[:100])
+	fmt.Print(justString)
 }
 func main() {
-	fmt.Println(utf8.RuneLen('🚗')) // 3
-	//someFunc()
+	fmt.Println(utf8.RuneLen('⛄')) // 3
+	someFunc()
 }
--- 22/main.go
+++ 22/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/big"
+)
 
+func main() {
+	a := new(big.Int)
+	if _, ok := a.SetString("480000000000000000000000", 10); !ok {
+		panic("can't create a number")
+	}
+
+	b := new(big.Int)
+	if _, ok := b.SetString("240000000000000000000000", 10); !ok {
+		panic("can't create a number")
+	}
+
+	res := new(big.Int)
+	fmt.Println("умножение:", res.Mul(a, b))
+	fmt.Println("деление:", res.Div(a, b))
+	fmt.Println("сложение:", res.Add(a, b))
+	fmt.Println("вычитание:", res.Sub(a, b))
 }
